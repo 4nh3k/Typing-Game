82be8d59b5f25b932c7ae3491d6459d60d59e82e
4nh3k <21520579@gm.uit.edu.vn>
2022-01-11 08:47:12 +0700

    Retry to push the project

---
+++ Typing Game/Typing Game/main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(900, 500), "SFML-Tutorial", sf::Style::Close);
+    window.setFramerateLimit(60);
+
+    sf::RectangleShape rectang(sf::Vector2f(400, 220));
+
+    rectang.setPosition(250, 140);
+    rectang.setFillColor(sf::Color::Yellow);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::EventType::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        window.draw(rectang);
+
+        window.display();
+    }
+
+    return 0;
+}
