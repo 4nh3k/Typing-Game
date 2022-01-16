e5d77558d42199e370ef73c00d0fd7c6960fff8a
nganngants <94037578+nganngants@users.noreply.github.com>
2022-01-16 11:47:42 +0700

    basic template

---
+++ Typing Game/Typing Game/Game.h
@@ -0,0 +1,51 @@
+#pragma once
+#pragma region Libararies and Namespaces
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <sstream>
+
+using namespace std;
+using namespace sf;
+
+#pragma endregion
+
+class Game
+{
+private:
+	// something for window
+	RenderWindow* window;
+	Event event;
+	Font font;
+	Vector2i mousePosWindow;
+	Vector2f mousePosView;
+	
+	// Game logic
+	unsigned points;
+	bool isEnd;
+
+	// functions for game initialization
+	void initVariables();
+	void initWindow();
+	void initFontandText();
+
+	// functions for game running
+	void pollEvents();
+	void updateMousePos();
+	void updateText();
+	void renderText();
+
+public:
+	//instructor, destructor
+	Game();
+	virtual ~Game();
+	void update();
+	void render();
+	const bool isRunning() const;
+	const bool endGame() const;
+};
