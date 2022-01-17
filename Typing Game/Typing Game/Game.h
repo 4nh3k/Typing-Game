37acf473e9f4a1ad916157576079d60d84e9841c
nganngants <94037578+nganngants@users.noreply.github.com>
2022-01-17 13:30:04 +0700

    Edit Game class again

--- Typing Game/Typing Game/Game.h
+++ Typing Game/Typing Game/Game.h
@@ -1,23 +1,13 @@
 #pragma once
-
-/*
-	@SOME NOTES
-
-	- Ctrl + . : create function from .h to .cpp
-	- Ctrl + K + C : comment
-	- Ctrl + K + U : uncomment
-
-*/
 #pragma region Libararies and Namespaces
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/System.hpp>
 #include <SFML/Network.hpp>
+#include <SFML/Window.hpp>
 #include <iostream>
-#include <vector>
 #include <ctime>
+#include <string>
 #include <sstream>
 
 using namespace std;
@@ -28,54 +18,34 @@ using namespace sf;
 class Game
 {
 private:
-	sf::Event event;
-	sf::VideoMode vidMode;
-	sf::Vector2i mousePosWindow;
-	sf::Vector2f mousePosView;
+	// something for window
+	RenderWindow* window;
+	Event event;
+	Font font;
+	Vector2i mousePosWindow;
+	Vector2f mousePosView;
+	
+	// Game logic
+	unsigned points;
+	bool isEnd;
 
-	//resources
-	sf::Font font;
-	sf::Text uiText;
-
-	//game logic
-	int health;
-	float enemySpawnTimer, enemySpawnTimerMax;
-	int maxEnemies;
-	bool mouseHeld;
-	bool endGame;
-
-	//game objects
-	sf::RectangleShape enemy;
-	vector <sf::RectangleShape> enemies;
-
+	// functions for game initialization
 	void initVariables();
 	void initWindow();
-	void initEnemy();
-	void initFont();
-	void initText();
+	void initFontandText();
 
+	// functions for game running
 	void pollEvents();
-
-	void spawnEnemy();
-	void updateEnemies();
+	void updateMousePos();
 	void updateText();
-	void updateMousePosition();
-
-	void renderEnemies(sf::RenderTarget& target);
-	void renderText(sf::RenderTarget& target);
+	void renderText();
 
 public:
-	RenderWindow* window;
-	unsigned points;
-
 	//instructor, destructor
-	Game(int difficulty);
+	Game();
 	virtual ~Game();
-	int difficulty;
-	//public functions for main
-	const bool running() const;
-	const bool isEndGame() const;
 	void update();
 	void render();
+	const bool isRunning() const;
+	const bool endGame() const;
 };
-
